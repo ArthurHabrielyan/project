0585bd797315fae29db5a47e5b38edc5130da57e
Arthur <zkdzkd227@gmail.com>
2021-01-07 20:59:11 +0200

    first task with js

---
+++ Udemy/js/script6.js
@@ -0,0 +1,19 @@
+let number = 4.6;
+
+console(4 / 0);
+
+const persone = "Alex";
+
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+
+//console.log(obj.name);
+console.log(obj["name"]);
+
+
+            0          1             2    3            4   5
+let arr = ["plum.png", "orange.jpg", "6", "apple.bmp", [], {}];
+console.log(arr[1]);
---
+++ Udemy/js/script7.js
@@ -0,0 +1,37 @@
+// "use strict";
+
+// //alert("Hello");
+
+// //const result = confirm("Are you here?");
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+// const answers = [];
+
+// // answers[0] = prompt("Как ваше имя?", "");
+// // answers[1] = prompt("Как ваша фамилия?", "");
+// // answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(typeof answers);
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+// let incr = 10,
+//   decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+const isCheked = true,
+  isClose = true;
+
+console.log(isCheked && isClose);
