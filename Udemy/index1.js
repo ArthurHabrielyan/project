0954da0c8dc45c4fcdf3a4ab3bfe19c9642bf7a8
Arthur <zkdzkd227@gmail.com>
2021-01-25 13:45:43 +0200

    3 task

--- Udemy/index1.js
+++ Udemy/index1.js
@@ -1,40 +1,13 @@
-// "use strict";
-
-// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
-
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   private: false,
-// };
-
-// for (let i = 0; i < 2; i++) {
-//   const a = prompt("Один из последних просмотренных фильмов?", ""),
-//     b = prompt("На сколько оцените его?", "");
-
-//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//     personalMovieDB.movies[a] = b;
-//     console.log("done");
-//   } else {
-//     console.log("error");
-//     i--;
-//   }
-// }
-
-// console.log(personalMovieDB);
-
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
-  privat: false,
+  private: false,
 };
 
 for (let i = 0; i < 2; i++) {
@@ -49,3 +22,5 @@ for (let i = 0; i < 2; i++) {
     i--;
   }
 }
+
+console.log(personalMovieDB);
--- Udemy/indexlection.js
+++ Udemy/indexlection.js
@@ -1,15 +1,33 @@
-if (4 == 9) {
-  console.log("Ok");
-} else {
-  console.log("Error");
+// if (4 == 9) {
+//   console.log("Ok");
+// } else {
+//   console.log("Error");
+// }
+
+// const kok = 24;
+
+// if (kok > 35) {
+//   console.log("false");
+// } else if (kok < 25) {
+//   console.log("false too");
+// } else {
+//   console.log("true");
+// }
+
+function showFirstMessage() {
+  console.log("Hello World");
 }
 
-const kok = 24;
+showFirstMessage();
 
-if (kok > 35) {
-  console.log("false");
-} else if (kok < 25) {
-  console.log("false too");
-} else {
-  console.log("true");
+function calc(a, b) {
+  return a + b;
 }
+
+console.log(calc(10, 6));
+
+const toto = function () {
+  console.log("Hello");
+};
+
+toto();
